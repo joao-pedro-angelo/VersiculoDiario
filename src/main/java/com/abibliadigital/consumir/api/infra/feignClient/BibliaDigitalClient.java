2d2d48892a3da4a94545da7212b5a59ca26fd7e5
my-github-2024 <carneiroangelojoaopedro@gmail.com>
2024-05-01 19:54:35 -0300

    Cliente OpenFeign - Interface

---
+++ src/main/java/com/abibliadigital/consumir/api/infra/feignClient/BibliaDigitalClient.java
@@ -0,0 +1,14 @@
+package com.abibliadigital.consumir.api.infra.feignClient;
+
+import com.abibliadigital.consumir.api.models.versiculos.VersiculoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "ablibiadigital", url = "https://www.abibliadigital.com.br/api/")
+public interface BibliaDigitalClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "verses/nvi/random")
+    VersiculoDTO retornaVersiculo();
+
+}
